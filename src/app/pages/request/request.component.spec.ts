da5f0b6e8919fe37af73b234ef04eaf864b60456
Basila Medjedovic <basilamedjedovic@gmail.com>
2023-12-14 20:21:30 +0100

    should create test for request component

--- src/app/pages/request/request.component.spec.ts
+++ src/app/pages/request/request.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RequestComponent } from './request.component';
+import { HttpClientModule } from '@angular/common/http'; 
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { of } from 'rxjs';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ConfirmationComponent } from 'src/app/confirmation/confirmation.component';
 
 describe('RequestComponent', () => {
   let component: RequestComponent;
@@ -9,6 +15,24 @@ describe('RequestComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [RequestComponent]
+      ,imports: [HttpClientModule, MatDialogModule]
+      ,providers: [
+        UserService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: {
+              subscribe: () => of({}), 
+              get: (param: string) => {
+               if (param === 'id') {
+                  return 'mockedUserId';
+                }
+                return 'someDefaultValue'; 
+              },
+            },
+          },
+        },
+      ],
     });
     fixture = TestBed.createComponent(RequestComponent);
     component = fixture.componentInstance;
